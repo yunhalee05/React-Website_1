21d4e95d70756014f9b30a9246d0039e2fcaf7ac
yunhalee05 <yunhalee0506@naver.com>
2021-01-29 20:33:46 +0900

    make website

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './App.css';
 import Navbar from'./components/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './components/Pages/Home';
+import Services from './components/Pages/Services';
+import Products from './components/Pages/Products';
+import SignUp from './components/Pages/SignUp';
 
 function App() {
   return (
@@ -9,7 +13,10 @@ function App() {
     <Router>
       <Navbar />
       <Switch>
-        <Route path='/' exact />
+        <Route path='/' exact component = {Home} />
+        <Route path='/services' component = {Services} />
+        <Route path='/products' component = {Products} />
+        <Route path='/sign-up' component = {SignUp} />
       </Switch>
     </Router>
     </>
